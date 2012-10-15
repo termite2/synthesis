f5bb092c138abd0c5f9503ee734668aa08ac1b43
Leonid Ryzhyk <leonidr@cse.unsw.edu.au>
2012-10-15 18:29:19 -0400

    more sane data types

--- AbsGame.hs
+++ AbsGame.hs
@@ -1,5 +1,8 @@
 module AbsGame(VarCategory(..),
+               Pred(..),
+               AbsVar(..),
                PredicateDB(..),
+               pdbPred,
                AbsGame(..)) where
 
 import Control.Monad.State
@@ -16,39 +19,55 @@ data VarCategory = VarState
 class (Show p, Eq p, Ord p) => Pred p where
     predOverlap :: p -> p -> Bool   -- True if predicates share a common variable
 
+data AbsVar p = PredVar {avarName::String, avarCat::VarCategory, avarTracked::Bool, avarPred::p}
+              | BoolVar {avarName::String, avarCat::VarCategory, avarTracked::Bool}
+              | EnumVar {avarName::String, avarCat::VarCategory, avarTracked::Bool, avarSize::Int}
+
+instance Eq (AbsVar p) where
+    (==) v1 v2 = avarName v1 == avarName v2
+
+instance Ord (AbsVar p) where
+    compate v1 v2 = compare (avarName v1) (avarName v2)
+
 -- Predicate database
 data PredicateDB c v p = PredicateDB { 
-    pdbCtx  :: c,                            -- Logic context used to allocate vars
-    pdbPred :: [(p, VarCategory)],           -- Predicates.  Do we need to store the tracked/untracked flag?
-    pdbBool :: [(String, VarCategory)],      -- Boolean variables.
-    pdbEnum :: [(String, VarCategory, Int)], -- Enum variables (name, number of values)
-    pdbVar  :: M.Map String v                -- Map predicate or variable name to logic variables
+    pdbCtx  :: c,                 -- Logic context used to allocate vars
+    pdbVar  :: M.Map (AbsVar p) v -- Map predicate or variable name to logic variables
 }
 
--- 
+pdbPred :: PredicateDB c v p -> [p]
+pdbPred pdb = map avarPred $ filter (\v -> case v of 
+                                                PredVar _ _ _ _ -> True
+                                                _               -> False) 
+                           $ pdbVar pdb
+
+-- Everything the abstraction algorithm needs to know about the game.
 data AbsGame c v a p = AbsGame { 
     -- Decompose goal relations into predicates and compile them.
     -- Returns the list of named goals.  The predicate DB contains
     -- newly discovered predicates and variables.
-    gameGoals      :: State (PredicateDB c v p) [(String,a)],
+    gameGoals       :: State (PredicateDB c v p) [(String,a)],
 
     -- Decompose fair set relations into predicates and compile them.
     -- Returns the list of fair sets.  The predicate DB contains
     -- newly discovered predicates and variables.
-    gameFair       :: State (PredicateDB c v p) [a],
+    gameFair        :: State (PredicateDB c v p) [a],
 
     -- Decompose initial state relation into predicates and compile it.
     -- In order to obtain an abstraction of the initial relation wrt predicate
     -- already in the DB, just quantify away all newly discovered predicates
     -- and variables.
-    gameInit       :: State (PredicateDB c v p) a,
+    gameInit        :: State (PredicateDB c v p) a,
 
-    -- Compute predicate update function.
-    gamePredUpdate :: p -> State (PredicateDB c v p) a,
+    -- Compute controllable and uncontrollable predicate update function.
+    gamePredUpdateC :: p -> State (PredicateDB c v p) a,
+    gamePredUpdateU :: p -> State (PredicateDB c v p) a,
 
-    -- Compute predicate update function.
-    gameBoolUpdate :: String -> State (PredicateDB c v p) a,
+    -- Compute boolean variable update function
+    gameBoolUpdateC :: String -> State (PredicateDB c v p) a,
+    gameBoolUpdateU :: String -> State (PredicateDB c v p) a,
 
     -- Compute enum variable update function.
-    gameEnumUpdate :: String -> State (PredicateDB c v p) a
+    gameEnumUpdateC :: String -> State (PredicateDB c v p) a,
+    gameEnumUpdateU :: String -> State (PredicateDB c v p) a
 }
