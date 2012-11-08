b7a7cb2e121ca717b8d330beeacf8984e6dddd11
Adam Walker <adam.walker@nicta.com.au>
2012-11-08 18:55:15 +1100

    PredicateDB specific to monadic bdd interface

--- PredicateDB.hs
+++ PredicateDB.hs
@@ -11,7 +11,14 @@ module PredicateDB(VarCategory(..),
                    pdbPutExt,
                    pdbGetExt) where
 
+import Control.Monad.ST.Lazy
+import qualified Data.Map as Map
+import Data.Map (Map)
+import Control.Monad.State
+import Safe
+
 import LogicClasses
+import CuddExplicitDeref
 
 data VarCategory = VarState
                  | VarTmp
@@ -32,65 +39,95 @@ instance Eq (AbsVar p) where
 instance Ord (AbsVar p) where
     compare v1 v2 = compare (avarName v1) (avarName v2)
 
+data PredDBState p o s u = PredDBState {
+    dbManager    :: STDdManager s u,
+    dbStatePreds :: Map p (DDNode s u),
+    dbStateVars  :: Map String [DDNode s u],
+    dbLabelPreds :: Map p (DDNode s u),
+    dbLabelVars  :: Map String [DDNode s u],
+    dbUserState  :: o,
+    dbNextIndex  :: Int
+}
+
 -- Predicate DB type:
 -- c - logic context
 -- v - logic variable
 -- p - predicate type
 -- o - opaque state stored by the predicate update algorithm in the PDB
-data PredicateDB c v p o x = PredicateDB
-
-instance Monad (PredicateDB c v p o) where
-    (>>=)  = undefined
-    return = undefined
-
----- Predicate database
---data PredicateDB c v p = PredicateDB { 
---    pdbCtx          :: c,                  -- Logic context used to allocate vars
---    pdbStateVar     :: M.Map (AbsVar p) v, -- Map predicate or variable name to logic variables
---    pdbTmpVar       :: M.Map (AbsVar p) v, -- Map predicate or variable name to logic variables
---    pdbUntrackedVar :: M.Map (AbsVar p) v  -- Map predicate or variable name to logic variables
---}
-
---pdbPred :: PredicateDB c v p -> [p]
---pdbPred pdb = map avarPred $ filter (\v -> case v of 
---                                                PredVar _ _ -> True
---                                                _           -> False) 
---                           $ M.keys (pdbStateVar pdb) ++ M.keys (pdbTmpVar pdb) ++ M.keys (pdbUntrackedVar pdb)
+type PredicateDB p o s u = StateT (PredDBState p o s u) (ST s)
 
 -- Return logic context
-pdbCtx :: PredicateDB c v p o c
-pdbCtx = undefined
+pdbCtx :: PredicateDB p o s u (STDdManager s u)
+pdbCtx = gets dbManager
 
 -- List all predicates
-pdbPred :: PredicateDB c v p o [p]
-pdbPred = undefined
+pdbPred :: PredicateDB p o s u [p]
+pdbPred = do
+    sp <- gets dbStatePreds
+    lp <- gets dbLabelPreds
+    return $ Map.keys sp ++ Map.keys lp
 
 -- List all abstract variables in PDB
-pdbAbsVar :: PredicateDB c v p o [AbsVar p]
+pdbAbsVar :: PredicateDB p o s u [AbsVar p]
 pdbAbsVar = undefined
 
 -- Retrieve existing var that is known to exist in the DB
-pdbGetVar :: AbsVar p -> PredicateDB c v p o v
-pdbGetVar = undefined
+pdbGetVar :: Ord p => AbsVar p -> PredicateDB p o s u [DDNode s u]
+pdbGetVar p = do
+    res <- pdbLookupVar p
+    return $ fromJustNote "pdbGetVar" res
+
+singleton x = [x]
 
 -- Lookup variable
-pdbLookupVar :: AbsVar p -> PredicateDB c v p o (Maybe v)
-pdbLookupVar = undefined
+pdbLookupVar :: Ord p => AbsVar p -> PredicateDB p o s u (Maybe [DDNode s u])
+pdbLookupVar (PredVar _ pred) = do
+    sp <- gets dbStatePreds
+    lp <- gets dbLabelPreds 
+    return $ liftM singleton $ Map.lookup pred (Map.union sp lp)
+pdbLookupVar (BoolVar name)   = do
+    sp <- gets dbStateVars
+    lp <- gets dbLabelVars
+    return $ Map.lookup name (Map.union sp lp)
+pdbLookupVar (EnumVar name _) = do
+    sp <- gets dbStateVars
+    lp <- gets dbLabelVars
+    return $ Map.lookup name (Map.union sp lp)
 
 -- Lookup or allocate variable
-pdbAllocVar :: AbsVar p -> VarCategory -> PredicateDB c v p o v
-pdbAllocVar = undefined
+pdbAllocVar :: (Ord p) => AbsVar p -> VarCategory -> PredicateDB p o s u [DDNode s u]
+pdbAllocVar (PredVar _ p)   VarState = something dbStatePreds p  (:[]) 1
+pdbAllocVar (PredVar _ p)   VarTmp   = something dbLabelPreds p  (:[]) 1
+pdbAllocVar (BoolVar nm)    VarState = something dbStateVars  nm id    1
+pdbAllocVar (BoolVar nm)    VarTmp   = something dbLabelVars  nm id    1
+pdbAllocVar (EnumVar nm sz) VarState = something dbStateVars  nm id    sz
+pdbAllocVar (EnumVar nm sz) VarTmp   = something dbLabelVars  nm id    sz
+
+--TODO actually put it in the map
+something accessor key func sz = do
+    theMap <- gets accessor
+    m      <- gets dbManager
+    case Map.lookup key theMap of
+        Just var -> return $ func var
+        Nothing -> do
+            st <- get
+            newVar <- lift $ sequence $ map (bvar m) (take sz $ iterate (+1) (dbNextIndex st))
+            modify $ \st -> st {dbNextIndex = dbNextIndex st + sz}
+            return newVar
+
 
 -- Allocate temporary logic variable 
 -- This variable is not part of the PDB and is only used internally
 -- by the variable update function computation algorithm
-pdbAllocTmpVar :: Int -> PredicateDB c v p o v
+pdbAllocTmpVar :: Int -> PredicateDB p o s u [DDNode s u]
 pdbAllocTmpVar = undefined
 
 -- Retrieve extended opaque state
-pdbGetExt :: PredicateDB c v p o o
-pdbGetExt = undefined
+pdbGetExt :: PredicateDB p o s u o
+pdbGetExt = gets dbUserState
 
 -- Update extended opaque state
-pdbPutExt :: o -> PredicateDB c v p o ()
-pdbPutExt = undefined
+pdbPutExt :: o -> PredicateDB p o s u ()
+pdbPutExt us = do
+    modify $ \st -> st {dbUserState = us}
+
