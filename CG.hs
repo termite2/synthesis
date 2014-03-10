783b49316c1a034cad2678108803b6d958590bd4
Adam Walker <adam.walker@nicta.com.au>
2014-03-10 12:45:42 -0400

    code generation work

--- CG.hs
+++ CG.hs
@@ -2,17 +2,22 @@
 
 module CG (
     SynthData(..),
-    pickLabel
+    availableLabels,
+    pickLabel,
+    ifCondition,
+    applyLabel
     ) where
 
+import Control.Monad
 import Control.Monad.ST
 
 import BddRecord
 import Interface
 
 data SynthData s u = SynthData {
-    sections    :: SectionInfo s u,
-    transitions :: [(DDNode s u, DDNode s u)]
+    sections                  :: SectionInfo s u,
+    transitions               :: [(DDNode s u, DDNode s u)],
+    uncontrollableTransitions :: [(DDNode s u, DDNode s u)]
 }
 
 data IteratorM m a = IteratorM {
@@ -40,8 +45,41 @@ ifCondition :: Ops s u -> SynthData s u -> DDNode s u -> DDNode s u -> ST s (May
 ifCondition Ops{..} SynthData{..} strategy stateSet = do
     undefined
 
+fixedPoint :: Ops s u -> (DDNode s u -> ST s (DDNode s u)) -> DDNode s u -> ST s (DDNode s u)
+fixedPoint ops@Ops{..} func start = do
+    res <- func start
+    deref start
+    case (res == start) of
+        True  -> return start
+        False -> fixedPoint ops func start
+
+forLoop start list func  = foldM func start list
+
+applyTrel :: Ops s u -> SynthData s u -> [(DDNode s u, DDNode s u)] -> DDNode s u -> ST s (DDNode s u)
+applyTrel Ops{..} SynthData{..} trel stateSet = do
+    let SectionInfo{..} = sections
+
+    combined <- forLoop bfalse trel $ \accum (cube, trel) -> do
+        constrained <- band trel stateSet
+        accum'      <- band constrained accum
+        deref constrained
+        deref accum
+        return accum'
+
+    sul' <- band _trackedCube _untrackedCube
+    sul  <- band sul' _labelCube
+    deref sul'
+
+    res <- bexists sul combined
+    deref sul
+    deref combined
+
+    return res
+
 --Given a label and a state, apply the label and then the transitive closure of uncontrollable transitions to compute the set of possible next states
 applyLabel :: Ops s u -> SynthData s u -> DDNode s u -> DDNode s u -> ST s (DDNode s u)
-applyLabel Ops{..} SynthData{..} stateSet label = do
-    undefined
+applyLabel ops@Ops{..} sd@SynthData{..} stateSet label = do
+    afterControllableAction    <- applyTrel ops sd transitions stateSet
+    afterUncontrollableActions <- fixedPoint ops (applyTrel ops sd uncontrollableTransitions ) afterControllableAction
+    return afterControllableAction
 
